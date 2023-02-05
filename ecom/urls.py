12795a765d4a451ad5381582ac827aefe22daa0f
gobind <gobindprasad@gmail.com>
2023-02-05 09:33:05 +0530

    added more html

--- ecom/urls.py
+++ ecom/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='product_list'),
     # path('', views.IndexClassView.as_view(), name='product_list'),
     path('add/', views.CreateItem.as_view(), name='create_item'),
+    # product list
     path('<int:pk>', views.ProductDetail.as_view(), name='details'),
     path('update/<int:id>/', views.update_item, name='update'),
 
